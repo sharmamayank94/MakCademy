6d32d923997400447f9477b3dd815e19980b2c3f
MAYANK <sharmamayank941@gmail.com>
2020-08-20 16:54:39 +0530

    host => connectionString

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express()
 knex = knex({
     client: 'pg',
     connection:{
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
